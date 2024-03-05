4b2f3bbb2bc484cda7c1e55d37726e9e31764724
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-05 21:11:57 +0000

    Delete else statement

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -73,7 +73,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
     os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+
     CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://.*\.gitpod\.io$",
     ]
