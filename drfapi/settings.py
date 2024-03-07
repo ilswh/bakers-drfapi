16bda0824f135a6a619ac5c0236b6e19d4012913
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-07 16:19:35 +0000

    comment out to find bug

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -153,9 +153,9 @@ if 'DEV' in os.environ:
          }
      }
 else:
-     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-     }
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 
