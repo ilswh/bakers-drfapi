f8df15b24e1a20ad285c0b1ec8bb8f4843a806a5
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-10 14:27:06 +0000

    Add bookmarks to posts serializer & view

--- posts/views.py
+++ posts/views.py
@@ -15,7 +15,8 @@ class PostList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
+        comments_count=Count('comment', distinct=True),
+        bookmarks_count=Count('bookmarks', distinct=True),
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
@@ -25,6 +26,7 @@ class PostList(generics.ListCreateAPIView):
     filterset_fields = [
         'owner__followed__owner__profile',
         'likes__owner__profile',
+        'bookmarks__owner__profile',
         'owner__profile',
     ]
     search_fields = [
@@ -33,8 +35,10 @@ class PostList(generics.ListCreateAPIView):
     ]
     ordering_fields = [
         'likes_count',
+        'bookmarks_count',
         'comments_count',
         'likes__created_at',
+        'bookmarks__created_at',
     ]
 
     def perform_create(self, serializer):
@@ -49,5 +53,6 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
+        comments_count=Count('comment', distinct=True),
+        bookmarks_count=Count('bookmarks', distinct=True),
     ).order_by('-created_at')
