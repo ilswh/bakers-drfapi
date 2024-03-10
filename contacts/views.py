36d487482b4176a770d8c352a9d475c50549f234
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-10 14:08:21 +0000

    Create views & urls for contact app

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+
+class ContactList(generics.ListCreateAPIView):
+    serializer_class = ContactSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Contact.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
