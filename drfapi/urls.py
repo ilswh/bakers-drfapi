a0287aa07acca73777500b2a8bc8b00efa7e754d
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-11 10:19:57 +0000

    Include contacts & bookmarks urls path in drf api

--- drfapi/urls.py
+++ drfapi/urls.py
@@ -32,4 +32,6 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
+    path('', include('contacts.urls')),
+    path('', include('bookmarks.urls')),
 ]
